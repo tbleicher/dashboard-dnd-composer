e2530136591c1caf78f817c006e136928713c451
Thomas Bleicher <thomas@white.space>
2020-04-13 10:11:38 +0100

    added TODO list

--- src/components/navigation/Sidebar.js
+++ src/components/navigation/Sidebar.js
@@ -2,27 +2,44 @@ import React from "react";
 import { Link, Stack, Text } from "@chakra-ui/core";
 
 import { ReactComponent as Logo } from "./logo.svg";
-const MenuItems = ({ active, children }) => {
+const MenuItem = ({ active, children, ...other }) => {
   if (active) {
     return (
-      <Text as="h3" display="block" my={2}>
+      <Text as="h3" display="block" my={2} {...other}>
         {children}
       </Text>
     );
   }
   return (
-    <Link display="block" my={2}>
+    <Link display="block" my={2} {...other}>
       {children}
     </Link>
   );
 };
 
+const SubMenuItem = (props) => {
+  return <MenuItem {...props} ml={4} fontSize="sm" my={1} />;
+};
+
 const Sidebar = (props) => {
   return (
-    <Stack as="nav" p={4} bg="brand.100" color="brand.700" {...props}>
-      <MenuItems active={true}>Dashboard</MenuItems>
-      <MenuItems>About</MenuItems>
-      <MenuItems>Blog</MenuItems>
+    <Stack
+      as="nav"
+      p={4}
+      bg="brand.100"
+      color="brand.700"
+      flexGrow={0}
+      flexShrink={0}
+      {...props}
+    >
+      <MenuItem active={true}>Dashboard</MenuItem>
+      <MenuItem>About</MenuItem>
+      <MenuItem fontWeight="bold">Todo:</MenuItem>
+      <SubMenuItem>clean up 'ADD_FRAME' action</SubMenuItem>
+      <SubMenuItem>add frame types</SubMenuItem>
+      <SubMenuItem>add Router</SubMenuItem>
+      <SubMenuItem>Dashboard edit state</SubMenuItem>
+      <SubMenuItem>layouts depending on screen size</SubMenuItem>
       <div style={{ flexGrow: 1 }} />
       <Logo fill="white" fillOpacity={0.6} />;
     </Stack>
