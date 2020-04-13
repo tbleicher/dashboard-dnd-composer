bb1832fcb1490bf7509879d5c01b86f5e6ac9982
Thomas Bleicher <thomas@white.space>
2020-04-13 19:25:13 +0100

    Updates

---
+++ precache-manifest.cb0f23d31d16e426997c0d06997f744b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b88814aa229684dd8f238201c03a67f",
+    "url": "/dashboard-dnd-composer/index.html"
+  },
+  {
+    "revision": "3c5b39ce2024314c517a",
+    "url": "/dashboard-dnd-composer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c4bd050f041789566b3f",
+    "url": "/dashboard-dnd-composer/static/js/2.300b1995.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dashboard-dnd-composer/static/js/2.300b1995.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c5b39ce2024314c517a",
+    "url": "/dashboard-dnd-composer/static/js/main.722a0722.chunk.js"
+  },
+  {
+    "revision": "e6050ea1a77b35685779",
+    "url": "/dashboard-dnd-composer/static/js/runtime-main.dc52f7e7.js"
+  },
+  {
+    "revision": "6019c3e6fee52d4c566528db31148ff3",
+    "url": "/dashboard-dnd-composer/static/media/logo.6019c3e6.svg"
+  }
+]);
